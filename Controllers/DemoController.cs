478ce8e33131e1842e36a1e55319fe3291dd2d52
AJaykumar15 <ajaysonaboina888@gmial.com>
2023-02-03 15:04:04 +0530

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoEx.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
